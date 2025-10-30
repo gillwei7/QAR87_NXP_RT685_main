31221a3c34655cca493493cdaff9bd74db5e74a2
Lydia Chen <lydia_chen@quantatw.com>
2025-10-30 17:41:55 +0800

    [BT_Audio][Power] Add charger and gauge functions to initial and get charging status and battery info

--- BT_Audio_with_full_audio_path/Rt685Conversa_handsfree_cm33/hal/hal_power.c
+++ BT_Audio_with_full_audio_path/Rt685Conversa_handsfree_cm33/hal/hal_power.c
@@ -5,5 +5,47 @@
  *      Author: Lydia
  */
 #if UsingQAR87Board == 1
+#include "hal_power.h"
+#include "bq256xx_charger.h"
+#include "glf70302.h"
+
+volatile bq256xx_status_t charger_status;
+volatile BatteryInfo battery;
+
+void hal_power_charger_bq25618_init(void)
+{
+	/* ============== Charger Init Start==============*/
+		bq256xx_cfg_t charger_cfg = {
+				.vindpm_uv = 4450000,
+				.iindpm_ua = 2000000,
+				.ichg_ua = 530000,
+				.vbatreg_uv = 4005000,
+				.iprechg_ua = 60000,
+				.iterm_ua = 20000,
+				.wdt_ms = 0
+		};
+		status_t bq_ret = bq256xx_init(&charger_cfg);
+		if ( bq_ret!= kStatus_Success) {
+			PRINTF("[Charger] bq256xx init failed!,ret:%d \n",bq_ret);
+
+		}
+		else{
+			PRINTF("[Charger] bq256xx initialized.OK \n");
+		}
+		bq256xx_write_reg(0x03, 0x31); // IPRECHG = 60mA, ITERM = 20mA
+	/* ============== Charger Init End==============*/
+}
+
+void hal_power_charger_bq25618_get_charging_status(void)
+{
+	bq256xx_poll_status(&charger_status);
+
+}
+
+
+void hal_power_gauge_glf70302_get_battery_level(void)
+{
+	glf70302_read_battery(&battery); //Read the battery level after powering on
+}
 
 #endif
