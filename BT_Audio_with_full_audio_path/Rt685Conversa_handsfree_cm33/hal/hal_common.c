0a23e6cc52f3374b0f5037f345eac35807449725
Lydia Chen <lydia_chen@quantatw.com>
2025-10-31 14:12:04 +0800

    [BT_Audio][GPIO] Add GPIO interrupt initialization using HAL_GpioInit() for touch, charger and gauge

--- BT_Audio_with_full_audio_path/Rt685Conversa_handsfree_cm33/hal/hal_common.c
+++ BT_Audio_with_full_audio_path/Rt685Conversa_handsfree_cm33/hal/hal_common.c
@@ -11,6 +11,7 @@
 #include "board.h"
 #include "button_handler.h"
 #include "i2c_component_handler.h"
+#include "fsl_adapter_gpio.h"
 
 /*******************************************************************************
  * Definitions
@@ -34,6 +35,13 @@ extern QueueHandle_t spi_request_queue ;
 extern EventGroupHandle_t spi_event_group ;
 extern SemaphoreHandle_t spi_semaphore ;
 
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+
+GPIO_HANDLE_DEFINE(s_TouchIntGpioHandle);
+GPIO_HANDLE_DEFINE(s_ChargerIntGpioHandle);
+GPIO_HANDLE_DEFINE(s_GaugeIntGpioHandle);
 
 /**
  * @description: Delay N us
@@ -113,6 +121,63 @@ static void hal_gpio_pin_init(void)
     PINT_EnableCallbackByIndex(BUTTON_PINT_BASE, kPINT_PinInt0);
     PINT_PinInterruptConfig(BUTTON_PINT_BASE, kPINT_PinInt1, kPINT_PinIntEnableBothEdges, pint_intr_callback);
     PINT_EnableCallbackByIndex(BUTTON_PINT_BASE, kPINT_PinInt1);
+
+}
+
+static void hal_gpio_interrupt_callback(void *param)
+{
+    BaseType_t xHPW = pdFALSE;
+
+    EventBits_t uxBitsToSet = (EventBits_t)param;
+
+    if (i2c_event_group)
+    {
+        xEventGroupSetBitsFromISR(i2c_event_group, uxBitsToSet, &xHPW);
+    }
+    portYIELD_FROM_ISR(xHPW);
+    SDK_ISR_EXIT_BARRIER;
+
+}
+
+void hal_gpio_interrupt_init(void)
+{
+    hal_gpio_pin_config_t touch_int_config = {
+        kHAL_GpioDirectionIn,
+        0,
+        NXP_TOUCH_INT_PORT,
+        NXP_TOUCH_INT_PIN,
+    };
+    hal_gpio_pin_config_t charger_int_config = {
+        kHAL_GpioDirectionIn,
+        0,
+        CHG_INT_N_R_PORT,
+        CHG_INT_N_R_PIN,
+    };
+
+    hal_gpio_pin_config_t gauge_int_config = {
+        kHAL_GpioDirectionIn,
+        0,
+        FG_INT_GLF70302_PORT,
+        FG_INT_GLF70302_PIN,
+    };
+
+    /* workaround for calling GPIO_PortInit may reset the configuration already done for the port */
+    CLOCK_EnableClock(kCLOCK_HsGpio1);
+    RESET_ClearPeripheralReset(kHSGPIO1_RST_SHIFT_RSTn);
+    CLOCK_EnableClock(kCLOCK_HsGpio2);
+    RESET_ClearPeripheralReset(kHSGPIO2_RST_SHIFT_RSTn);
+
+    HAL_GpioInit(s_TouchIntGpioHandle, &touch_int_config);
+    HAL_GpioInit(s_ChargerIntGpioHandle, &charger_int_config);
+    HAL_GpioInit(s_GaugeIntGpioHandle, &gauge_int_config);
+
+    HAL_GpioSetTriggerMode(s_TouchIntGpioHandle, kHAL_GpioInterruptFallingEdge);
+    HAL_GpioSetTriggerMode(s_ChargerIntGpioHandle, kHAL_GpioInterruptFallingEdge);
+    HAL_GpioSetTriggerMode(s_GaugeIntGpioHandle, kHAL_GpioInterruptFallingEdge);
+
+    HAL_GpioInstallCallback(s_TouchIntGpioHandle, hal_gpio_interrupt_callback, (void *)TOUCH_EVENT_BIT);
+    HAL_GpioInstallCallback(s_ChargerIntGpioHandle, hal_gpio_interrupt_callback, (void *)CHARGER_EVENT_BIT);
+    HAL_GpioInstallCallback(s_GaugeIntGpioHandle, hal_gpio_interrupt_callback, (void *)GAUGE_EVENT_BIT);
 
 }
 
