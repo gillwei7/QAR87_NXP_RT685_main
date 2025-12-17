310ebe9a0a991a3ff0e7e122d2011cb616a1d0fe
Gill Wei (魏鴻憲) <Gill.Wei@quantatw.com>
2025-12-17 18:32:02 +0800

    [BT_Audio][Power] Fix Disable Power Down error
    define ENABLE_POWER_DOWN 1

--- BT_Audio_with_full_audio_path/Rt685Conversa_handsfree_cm33/source/GlobalDef.h
+++ BT_Audio_with_full_audio_path/Rt685Conversa_handsfree_cm33/source/GlobalDef.h
@@ -172,7 +172,7 @@ SEMA42_Unlock(APP_SEMA42, SEMA42_GATE0);			\
 //gill
 #define AUTO_CONNECT_ENABLE     1
 #define BT_CONNECTION_LOG       0
-#define ENABLE_POWER_DOWN       0
+#define ENABLE_POWER_DOWN       1
 
 
 #endif
