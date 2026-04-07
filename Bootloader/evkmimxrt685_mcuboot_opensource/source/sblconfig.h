fc4b92e9fe6fa7151da54d3f85a23814f232751d
Gill Wei (魏鴻憲) <Gill.Wei@quantatw.com>
2026-04-07 11:17:30 +0800

    [OTA]Reupload the evkmimxrt685_mcuboot_opensource directory
    Purpose: Since the commit dd831ee4 actually not upload the mcuboot_opensource
    directory, and git can not detect the change of mcuboot_opensource
    Action: Change the name of directory and upload again

---
+++ Bootloader/evkmimxrt685_mcuboot_opensource/source/sblconfig.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2021 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef SBL_CONFIG_H__
+#define SBL_CONFIG_H__
+
+/* Board specific register for flash remap functionality */
+#define FLASH_REMAP_START_REG           0x40134420      /* RW61x flash remap start address register */
+#define FLASH_REMAP_END_REG             0x40134424      /* RW61x flash remap end address register */
+#define FLASH_REMAP_OFFSET_REG          0x40134428      /* RW61x flash remap offset register */
+
+/*******************************************************************/
+/* Use default configuration if setup from Kconfig is not provided */
+/*******************************************************************/
+#ifndef CONFIG_BOOT_CUSTOM_DEVICE_SETUP
+
+/* MCUBoot Flash Config */
+
+#define CONFIG_MCUBOOT_MAX_IMG_SECTORS 800
+
+/*
+ * MCUBoot upgrade mode
+ *
+ * The default MCUBoot configuration is to use swap mechanism. In case the flash
+ * remapping functionality is supported by processor the alternative mechanism
+ * using direct-xip mode can be used and evaluated by user.
+ */
+#define CONFIG_MCUBOOT_FLASH_REMAP_ENABLE
+
+/* Crypto Config */
+
+#define CONFIG_BOOT_SIGNATURE
+#define CONFIG_BOOT_SIGNATURE_TYPE_RSA
+#define CONFIG_BOOT_SIGNATURE_TYPE_RSA_LEN 2048
+#define COMPONENT_MBEDTLS
+#define CONFIG_BOOT_BOOTSTRAP
+
+#endif /* CONFIG_BOOT_CUSTOM_DEVICE_SETUP */
+
+#endif
