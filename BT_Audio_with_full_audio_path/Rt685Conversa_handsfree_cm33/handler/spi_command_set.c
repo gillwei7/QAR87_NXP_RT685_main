0d2cc715fd24827a4e235d83c5034403de0e548b
Daryl Chung <daryl.chung@quantatw.com>
2026-04-22 18:42:08 +0800

    [SPI][Feature] Add SPI command atomic event : WIFI_AP_OPEN, WIFI_AP_CLOSE.

--- BT_Audio_with_full_audio_path/Rt685Conversa_handsfree_cm33/handler/spi_command_set.c
+++ BT_Audio_with_full_audio_path/Rt685Conversa_handsfree_cm33/handler/spi_command_set.c
@@ -387,6 +387,14 @@ void spi_command_atomic_event_parser (uint8_t event_id, const uint8_t *args)
 			PRINTF("[SPI][Event] OTA_FINISHED \r\n ");
 			break;
 
+		case CMD_ATOMIC_EVENT_WIFI_AP_OPEN:
+			PRINTF("[SPI][Event] WIFI_AP_OPEN \r\n ");
+			break;
+
+		case CMD_ATOMIC_EVENT_WIFI_AP_CLOSE:
+			PRINTF("[SPI][Event] WIFI_AP_CLOSE \r\n ");
+			break;
+
 		case CMD_ATOMIC_EVENT_UNKNOWN_CMD_ERROR:
 			PRINTF("[SPI][Event] UNKNOWN_CMD_ERROR \r\n ");
 			break;
